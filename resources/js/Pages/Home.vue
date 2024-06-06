a011fe605b9d2a39a07f1511beb53174c60d5951
MAkbarD <mad4869@gmail.com>
2024-06-06 19:16:48 +0700

    Initial commit

---
+++ resources/js/Pages/Home.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts">
+import { Head, Link } from '@inertiajs/vue3';
+
+import Layout from '@/Layouts/Layout.vue';
+</script>
+
+<template>
+
+    <Head title="Home" />
+    <div>
+        <Layout>
+            <h1>Home</h1>
+        </Layout>
+    </div>
+</template>
